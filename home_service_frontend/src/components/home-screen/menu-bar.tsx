b6d04759d6f35502cb206b839a3031f06f3025cb
jordanovx <jordanov2295@gmail.com>
2022-04-28 11:05:27 +0200

    ui improvments

--- home_service_frontend/src/components/home-screen/menu-bar.tsx
+++ home_service_frontend/src/components/home-screen/menu-bar.tsx
@@ -1,6 +1,9 @@
-import { HStack, Text, Button, VStack } from "@chakra-ui/react";
+import { HStack, Text, Button, VStack, Flex, Spacer, Image, Icon, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { PageContent } from "./page-content";
+//import Logo from "../../../public/icons/logo.svg"
+import { Logo } from "../../../public/icons/logo";
+
 
 const MenuBar = () => {
   const router = useRouter();
@@ -9,14 +12,34 @@ const MenuBar = () => {
 
   return (
     <VStack spacing={24} color={'white'}>
-      <HStack justify="center">
-        <HStack h="full" w="full" justify="center" spacing={5} m={2}>
-          <Text>Најди помошник</Text>
-          <Text>Клиент</Text>
-          <Text>Зошто ние?</Text>
-          <Text>Контакт</Text>
-          <HStack pl={6} justify="flex-end">
+      <HStack justify="center" width="full">
+        {/* <HStack h="full" w="full" justify="center" spacing={170} m={2}> */}
+          <Box  w="full" h="full">
+          <Flex w="full" gap={30} p={3} >
+          <Box w='220px' >
+            <Logo />
+          </Box>
+   
+          <Spacer />
+          <Box >
+            <Text m={2} p={0}>Најди помошник</Text>
+          </Box>
+          <Spacer w={30} />
+          <Box>
+            <Text m={3}>Клиент</Text>
+          </Box>
+          <Spacer />
+          <Box>
+          <Text m={3}>Зошто ние?</Text>
+          </Box>
+          <Spacer  />
+          <Box>
+          <Text m={3}>Контакт</Text>
+          </Box>
+          <Spacer />
+          <Box>
             <Button
+            
               _hover={{ bg: "#6B774B" }}
               variant="ghost"
               //isDisabled={isUserLoggedIn}
@@ -24,6 +47,9 @@ const MenuBar = () => {
             >
               Најава 
             </Button>
+            
+          </Box>
+          <Box>
             <Button
               bgColor={"#93A560"}
               _hover={{ bg: "" }}
@@ -31,8 +57,15 @@ const MenuBar = () => {
             >
               Регистрација 
             </Button>
-          </HStack>
-        </HStack>
+          </Box>
+          
+
+          </Flex>
+          </Box>
+
+   
+          
+        {/* </HStack> */}
       </HStack>
       <PageContent />
     </VStack>
